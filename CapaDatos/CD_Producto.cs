824a1e87925f7b9873a0f60b7d7f354c1308bc68
Barny <ismaquesada23@icloud.com>
2025-04-03 18:18:49 -0600

    master

---
+++ CapaDatos/CD_Producto.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Capa_Entidad;
+using Capa_Interfaces;
+
+namespace Capa_Datos
+{
+    public class CD_Producto : IProducto
+    {
+        public Task<IEnumerable<Producto>> ObtenerProductos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Capa_Interfaces/IProducto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Capa_Entidad;
+ 
+
+namespace Capa_Interfaces
+{
+    public  interface IProducto
+    {
+        Task<IEnumerable<Producto>> ObtenerProductos();
+    }
+}
